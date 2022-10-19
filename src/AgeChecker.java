304df51c5e1baaf8284af96f1ce263a4fcc4549a
korneliafijorek <kornelia.fijorek@gmail.com>
2022-10-19 20:50:09 +0200

    sample commit message

--- src/AgeChecker.java
+++ src/AgeChecker.java
@@ -4,13 +4,14 @@ public class AgeChecker {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj swoje imię.");
+        System.out.println("Podaj swój wiek");
         int age = scanner.nextInt();
-        if(age < 18){
-            System.out.println("Nie możesz kupić alkoholu!");
-        }
-        else {
+        if(age >=18){
             System.out.println("Dziękuję za zakup. :)");
+        } else if (age > 0){
+            System.out.println("Nie możesz kupić alkoholu!");
+        } else {
+            System.out.println("Podanną niepoprawnną liczbę (np. ujemną).");
         }
     }
 }
---
+++ src/Pętle.java
@@ -0,0 +1,22 @@
+public class Pętle {
+    public static void main(String[] args) {
+        int number = 60;
+
+        for(int i = 0; i < 10; i++){
+            //System.out.println("Wartość i: " + i);
+        }
+
+        int number1 = 0;
+        while(number1 < 20){
+            //System.out.println("Moja wartość to :" + number1 + " i jest mniejsza od 20");
+            number1++;
+        }
+
+        int number2 = 10;
+        do{
+            System.out.println("Moja wartość to :" + number2 + " i jest mniejsza od 20");
+            number2++;
+        }while(number2<20);
+
+    }
+}
